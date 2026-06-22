d0e55ed72df34f9eed9b176c9bdb34a2df2beed0
Ashton <westashton416@gmail.com>
2026-06-22 03:04:24 +0100

    created superuser and tested

---
+++ backend/accounts/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+#Extends Django's user model to add different roles (student/teacher/admin)
+
+class User(AbstractUser):
+#User roles
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('teacher', 'Teacher'),
+        ('admin', 'Admin'),
+    ]
+#Stores user roles
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='student'
+    )
+
+    def __str__(self):
+        return self.username
